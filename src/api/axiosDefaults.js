d9312fa4edfdfa2bfea8c8906ed1194943685a0e
MikeMaroni9 <k.vaitka@gmail.com>
2023-10-24 22:42:19 +0000

    fixed

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://fisntagram-f2918720a69f.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api9-921f22751f01.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
